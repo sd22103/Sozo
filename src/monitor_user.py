f648b359b52043f23311a176d34787817fdcb8a4
sd22103 <sd22103@toyota-ti.ac.jp>
2024-12-09 17:08:29 +0900

    feat: 開始時と終了時に音声追加

--- src/monitor_user.py
+++ src/monitor_user.py
@@ -1,7 +1,7 @@
 import time
 import traceback
 
-def monitor_user(ultrasonic_sensor, led, shared_state, verbose=False):
+def monitor_user(ultrasonic_sensor, led, speaker, shared_state, const, verbose=False):
     """Monitor user and update shared state accordingly.
 
     Parameters
@@ -10,8 +10,14 @@ def monitor_user(ultrasonic_sensor, led, shared_state, verbose=False):
         Ultrasonic sensor object.
     led : object of class LED
         LED object.
+    speaker : object of class Speaker
+        Speaker object.
     shared_state : dict
         Shared state between threads.
+    const : AttrDict
+        Constants object.
+    verbose : bool, optional
+        Whether to print debug messages. The default is False.
     """
     left_count = 0
     print("ユーザー検知開始")
@@ -25,10 +31,15 @@ def monitor_user(ultrasonic_sensor, led, shared_state, verbose=False):
                 print(f"distance={distance}cm")
             if distance < 150:
                 shared_state["human_detected"] = True
+                speaker.play_audio(const.start_audio_file)
                 led.on()
                 left_count = 0
+                led_flag = 1
             else:
                 left_count += 1
+                if led_flag == 1:
+                    speaker.play_audio(const.finish_audio_file)
+                    led_flag = 0
                 if left_count > 5:
                     shared_state["human_detected"] = False
                     led.off()
